43cf66887c8758ab79bdb7d3842d82c2b67477f8
dmc490 <tzviru@gmail.com>
2016-10-11 03:36:58 +0300

    failed to ignore js files in bin folder. WTF?

---
+++ bin/TriPascal.js
@@ -0,0 +1,16 @@
+function printTri(depth){
+    let TriArray = [[1]];
+    for (var row=1;row<depth;row++){
+        TriArray.push([]);
+        let ansectorRow = TriArray[row-1];
+        for(var col=0;col<row + 1;col++){
+            TriArray[row].push([0]);
+            var rightValue = col-1 > -1 ? ansectorRow[col-1]: 0 ;
+            var leftValue = col < ansectorRow.length ? ansectorRow[col] : 0;
+
+            TriArray[row][col] =+ rightValue + leftValue;
+        }
+    }
+    return TriArray;
+}
+console.log(printTri(5));

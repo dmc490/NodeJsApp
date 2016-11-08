2c16fefc1e23bffd58f0e49fd8bde265bcc95559
dmc490 <tzviru@gmail.com>
2016-11-08 20:41:18 +0200

    add basic react app add webpack of the react app update gitignore and package.json accordingly

---
+++ src/js/components/Footer.js
@@ -0,0 +1,13 @@
+/**
+ * Created by userr on 06/11/2016.
+ */
+import React from "react";
+
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <footer>footer</footer>
+        );
+    }
+}
